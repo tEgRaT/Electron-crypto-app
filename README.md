8784b3fa10a513ba7b6f8246b28b5007c7fab428
tEgRaT <caoguanghong@hotmail.com>
2018-04-24 02:38:57 +0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Electron crypto app
+
+npm install electron --save-dev --save-exact
+
+Copy some code from boiler plate on page https://electronjs.org/docs/tutorial/first-app will make it easy to get start.
---
+++ src/add.js
@@ -0,0 +1,20 @@
+const electron = require('electron')
+const path = require('path')
+const remote = electron.remote
+const ipc = electron.ipcRenderer
+
+const closeBtn = document.getElementById('closeBtn')
+
+closeBtn.addEventListener('click', function (event) {
+    let window = remote.getCurrentWindow()
+
+    window.close()
+})
+
+const updateBtn = document.getElementById('updateBtn')
+updateBtn.addEventListener('click', () => {
+    ipc.send('update-notify-value', document.getElementById('notifyVal').value)
+
+    let window = remote.getCurrentWindow()
+    window.close()
+})
